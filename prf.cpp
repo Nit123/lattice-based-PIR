7460ba9c10c51ebbae6e73bea4316c1770dc4aef
Nit123 <karthanitesh@gmail.com>
2022-01-19 11:44:40 -0600

    Actually added modulus to the calculations!

--- prf.cpp
+++ prf.cpp
@@ -7,11 +7,11 @@
 #include <sstream>
 
 
-#define M 1000 // change to 500 for long long int version
-#define N 500 // change to 250 for long long int version
-#define p 65535 //(2^16-1)
-#define q 4294967296 // (2^32)...
-#define ratio q/p
+#define M 1000 // change to 500 for uint64_t version
+#define N 500 // change to 250 for uint64_t version
+#define p 4294967296 //(2^16-1)
+#define q 65535 // (2^32)...
+#define ratio p/q
 
 
 
@@ -20,35 +20,36 @@ using namespace std;
 /*
     INTEGER VERSION OF THE PRF: requires M and N to be smaller 
 */
-long long int* prf(long long int A[N][M], long long int x[M]); // main PRF function
-void initializeArrays(long long int A[N][M], long long int x[M]); // initializes random arrays
+uint64_t* prf(uint64_t A[N][M], uint64_t x[M]); // main PRF function
+void initializeArrays(uint64_t A[N][M], uint64_t x[M]); // initializes random arrays
 void testIntegerPRF(); // tester
 
 
-/*
-    FLOATING POINT VERSION OF PRF: M and N can be large but requires matrices to be outputted and read to a file
-*/
-void initializeArraysIntoFiles(); // Initialize random arrays into files
-double* prf_from_files(); // PRF that reads from files
-void testRealPRF(); // tester
+// /*
+//     FLOATING POINT VERSION OF PRF: M and N can be large but requires matrices to be outputted and read to a file
+// */
+// void initializeArraysIntoFiles(); // Initialize random arrays into files
+// double* prf_from_files(); // PRF that reads from files
+// void testRealPRF(); // tester
 
 int main(int argc, char *argv[]){
-    //testIntegerPRF();
-    testRealPRF();
+    testIntegerPRF();
+    //testRealPRF();
 
     return 0;
 }
 
 void testIntegerPRF(){
-    long long int A[N][M] = {};
-    long long int x[M] = {};
+    //uint64_t A[N][M] = {};
+    auto A = new uint64_t[N][M];
+    uint64_t *x = new uint64_t[M];
 
     cout << "Initializing arrays" << endl;
     initializeArrays(A, x);
     cout << "Finished initializing arrays" << endl;
 
     cout << "Beginning prf" << endl;
-    long long int* randomNum = prf(A, x);
+    uint64_t* randomNum = prf(A, x);
     cout << "end of prf" << endl << endl;
 
     for(int i = 0; i < N; i++){
@@ -58,47 +59,47 @@ void testIntegerPRF(){
     return;
 }
 
-void testRealPRF(){
-    initializeArraysIntoFiles();
-    double *randomNum = prf_from_files();
+// void testRealPRF(){
+//     initializeArraysIntoFiles();
+//     double *randomNum = prf_from_files();
 
-     for(int i = 0; i < N; i++){
-        cout << randomNum[i] << " ";
-    }
+//      for(int i = 0; i < N; i++){
+//         cout << randomNum[i] << " ";
+//     }
 
-    return;
-}
+//     return;
+// }
 
-void initializeArraysIntoFiles(){
+// void initializeArraysIntoFiles(){
+//     random_device rd;
+//     std::mt19937 gen(rd());
+//     uniform_int_distribution<uint64_t> distr(0, q);
+//     cout << "Initializing arrays and placing into files" << endl;
+//     ofstream A_file, x_file;
+//     A_file.open("A.txt");
+//     for(int i = 0; i < N; ++i){
+//         for(int j = 0; j < M; ++j){
+//             A_file << distr(gen) << " ";
+//         }
+
+//         A_file << endl;
+//     }
+
+//     A_file.close();
+
+//     x_file.open("x.txt");
+//     for(int i = 0; i < M; i++){
+//         x_file << distr(gen) << " ";
+//     }
+
+//     x_file.close();
+//     return;
+// }
+
+void initializeArrays(uint64_t A[N][M], uint64_t x[M]){
     random_device rd;
     std::mt19937 gen(rd());
-    uniform_int_distribution<long long int> distr(0, q);
-    cout << "Initializing arrays and placing into files" << endl;
-    ofstream A_file, x_file;
-    A_file.open("A.txt");
-    for(int i = 0; i < N; ++i){
-        for(int j = 0; j < M; ++j){
-            A_file << distr(gen) << " ";
-        }
-
-        A_file << endl;
-    }
-
-    A_file.close();
-
-    x_file.open("x.txt");
-    for(int i = 0; i < M; i++){
-        x_file << distr(gen) << " ";
-    }
-
-    x_file.close();
-    return;
-}
-
-void initializeArrays(long long int A[N][M], long long int x[M]){
-    random_device rd;
-    std::mt19937 gen(rd());
-    uniform_int_distribution<long long int> distr(0, p-1);
+    uniform_int_distribution<uint64_t> distr(0, q);
     cout << "in Initialize Arrays" << endl;
     for(int i = 0; i < N; ++i){
         for(int j = 0; j < M; ++j){
@@ -111,82 +112,82 @@ void initializeArrays(long long int A[N][M], long long int x[M]){
     }
 }
 
-double* prf_from_files(){
-    double x[M] = {};
+// double* prf_from_files(){
+//     double x[M] = {};
 
-    long long int temp_val;
-    int i = 0;
-    ifstream x_file;
-    x_file.open("x.txt");
-    while(x_file >> temp_val){
-        x[i] = temp_val;
-        i++;
-    }
-    x_file.close();
+//     uint64_t temp_val;
+//     int i = 0;
+//     ifstream x_file;
+//     x_file.open("x.txt");
+//     while(x_file >> temp_val){
+//         x[i] = temp_val;
+//         i++;
+//     }
+//     x_file.close();
 
 
-    static double rand[N];
-    //double ratio = q / p;
+//     static double rand[N];
+//     //double ratio = q / p;
 
-    // double temp_product = 0;
-    double row_vector[M] = {};
+//     // double temp_product = 0;
+//     double row_vector[M] = {};
 
-    string line;
-    ifstream A_file;
-    int count = 0;
-    int linecount = 0;
-    A_file.open("A.txt");
-    while(A_file){
-        getline(A_file, line);
-        stringstream stream(line);
+//     string line;
+//     ifstream A_file;
+//     int count = 0;
+//     int linecount = 0;
+//     A_file.open("A.txt");
+//     while(A_file){
+//         getline(A_file, line);
+//         stringstream stream(line);
         
-        long long int element;
+//         uint64_t element;
 
-        while(stream >> element){
-            row_vector[count] = element;
-            count++;
-        }
-        count = 0;
+//         while(stream >> element){
+//             row_vector[count] = element;
+//             count++;
+//         }
+//         count = 0;
 
 
-        // do the calculation you need for this line
-        for(int i = 0; i < M; i++){
-            rand[linecount] += (row_vector[i] * x[linecount]);
-        }
+//         // do the calculation you need for this line
+//         for(int i = 0; i < M; i++){
+//             rand[linecount] += (row_vector[i] * x[linecount]);
+//         }
 
-        cout << "Start rounding" << endl;
-        // rounding protocol
-        double count2 = 1;
-        while(count2 * ratio < rand[linecount]){
-            count2 *= 2;
-        }
+//         cout << "Start rounding" << endl;
+//         // rounding protocol
+//         double count2 = 1;
+//         while(count2 * ratio < rand[linecount]){
+//             count2 *= 2;
+//         }
 
-        rand[linecount] = round((count2-1) * ratio);
-        count2 = 1;
+//         rand[linecount] = round((count2-1) * ratio);
+//         count2 = 1;
 
-        linecount++;
-        //count++;
-        cout << "end rounding" << endl;
-    }
+//         linecount++;
+//         //count++;
+//         cout << "end rounding" << endl;
+//     }
 
-    return rand;
-}
+//     return rand;
+// }
 
-long long int* prf(long long int A[N][M], long long int x[M]){
+uint64_t* prf(uint64_t A[N][M], uint64_t x[M]){
     //cout << "in PRF" << endl;
-    static long long int rand[N];
+    static uint64_t rand[N];
     //double ratio = q/p;
-    double temp = 0;
+    uint64_t temp = 0;
 
     for(int i = 0; i < N; ++i){
         for(int j = 0; j < M; ++j){
-            temp += (A[i][j] * x[j]);
+            temp = temp + ((A[i][j] * x[j]) % p);
         }
 
         // // // rounding to p
         //     // // dumb way: x = rand[i]
         //     // // ratio while loop until we get the answer
-            int count = 1;
+            uint64_t count = 1;
 
             cout << "Start rounding" << endl;
             while(count *  ratio < temp){
