b1e4a749efad71c76419e691009f76bb74353034
Nitesh Kartha <nk9223@cs.utexas.edu>
2022-01-29 14:43:03 -0600

    Got AES-PRG to work! Need to get it into prf.cpp

--- main.cpp
+++ main.cpp
@@ -14,16 +14,76 @@ void print(unsigned __int128 x) {
     putchar(x % 10 + '0');
 }
 
-int main(){
-	PseudoRandomGenerator prg;
-	__int128 seed = 1000;
+void print_key(AES_KEY &key){
+  cout << "Printing key in blocks" << endl;
+  for(auto item: key.rd_key){
+    print((unsigned __int128) item);
+    cout << endl;
+  }
+  cout << "Done printing key" << endl;
+}
 
-	__int128 test = prg.pm_bounded(seed);
-	//cout << sizeof(test) << endl;
-	print((unsigned) test);
+void bin(__int128 n)
+{
+    /* step 1 */
+    if (n > 1)
+        bin(n / 2);
+ 
+    /* step 2 */
+    n = n %2;
+    print((unsigned __int128) n);
+}
+
+  void bin(uint64_t n)
+{
+    /* step 1 */
+    if (n > 1)
+        bin(n / 2);
+ 
+    /* step 2 */
+    cout << n % 2;
+}
+
+int main(){
+	// PseudoRandomGenerator prg;
+	// __int128 seed = 1000;
+
+	// __int128 test = prg.pm_bounded(seed);
+	// //cout << sizeof(test) << endl;
+	// print((unsigned) test);
   //cout << sizeof(intmax_t) << endl;
   //cout << sizeof(uintmax_t) << endl;
   // __int128 test1 = 14;
   // unsigned __int128 test2 = -14;
+  byte buffer[16];
+  static ifstream urandom("/dev/urandom", std::ios::binary);
+  urandom.read(reinterpret_cast<char *>(buffer), 16);
+  urandom.close();
+
+  AES_KEY aes_key{};
+
+  //print_key(aes_key);
+  AES_128_Key_Expansion(buffer, &aes_key);
+  //print_key(aes_key);
+
+  block blk;
+  //print((unsigned __int128)blk);
+  //cout << endl;
+  AES_ecb_encrypt_blk(&blk, &aes_key);
+  __int128 num = (__int128) blk;
+  cout << "num: ";
+  bin(num);
+  uint64_t val = 0;
+  val = val | num;
+  cout << "val1: ";
+  bin(val);
+  //cout << "val: " << val << endl;
+  val = 0;
+  num = num >> 64;
+  val = val | num;
+  cout << "val2: ";
+  bin(val);
+  //cout << "val2: " << val << endl; 
+  //print((unsigned __int128)blk);
 }
 
